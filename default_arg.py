d25bcb85163c1d4029b9075df2116a21a824ce61
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2024-04-17 18:01:53 -0500

    Add shadowing and default argument examples

---
+++ default_arg.py
@@ -0,0 +1,9 @@
+def append_1_and_print(nums=[]):
+    nums.append(1)
+    print(nums)
+
+
+# call with default argument for nums
+append_1_and_print()
+append_1_and_print()
+append_1_and_print()
---
+++ shadowing.py
@@ -0,0 +1,9 @@
+x = 10
+
+
+def print_x():
+    x += 5
+    print(x)
+
+
+print_x()
